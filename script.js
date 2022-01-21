ed85330aa1a8d8a0ece1d92729730b29f9c7f20e
Priyanshi Sharma <goodgameempireliona@gmail.com>
2022-01-21 11:30:56 +0530

    project v1.0

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function show(){
+    var output = document.getElementById("result");
+    output.style.display = "block";
 }
-        
+
+function display_rate(){
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("enteredRate");
+    output.innerHTML = slider.value + "%";
+}
+
+function compute(){
+    var p = Number(document.getElementById("principal").value);
+    var r = Number(document.getElementById("rate").value);
+    var t = Number(document.getElementById("years").value);
+    var s = p*r*t/100;
+    var y = new Date().getFullYear() + t;
+    document.getElementById("p").innerHTML = "" + p;
+    document.getElementById("r").innerHTML = "" + r;
+    document.getElementById("s").innerHTML = "" + s;
+    document.getElementById("y").innerHTML = "" + y;
+    show();
+}
